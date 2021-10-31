787c2c3a76a3390af39404870ce521b856f71565
ItsRicmor <ricardojms1999@gmail.com>
2021-10-31 16:01:37 -0600

    Upgrade native-base dependency

--- App.js
+++ App.js
@@ -1,39 +1,22 @@
 /* eslint-disable @typescript-eslint/camelcase */
-import React, { useEffect, useState } from 'react';
-import * as Font from 'expo-font';
-import { Ionicons } from '@expo/vector-icons';
-import AppLoading from 'expo-app-loading';
-import { Provider } from 'react-redux';
 import { NavigationContainer } from '@react-navigation/native';
-import { rootStore } from './src/stores/rootStore';
+import { NativeBaseProvider } from 'native-base';
+import React from 'react';
+import { Provider } from 'react-redux';
 import { AppNavigator } from './src/navigation';
+import { rootStore } from './src/stores/rootStore';
 
 const initialState = {};
 const store = rootStore(initialState);
 
-const App = () => {
-  const [isReady, setIsReady] = useState(false);
-  useEffect(() => {
-    const asyncFonts = async () => {
-      await Font.loadAsync({
-        Roboto: require('native-base/Fonts/Roboto.ttf'),
-        Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
-        ...Ionicons.font,
-      });
-      setIsReady(true);
-    };
-    asyncFonts();
-  }, []);
-  if (!isReady) {
-    return <AppLoading />;
-  }
-  return (
+const App = () => (
+  <NativeBaseProvider>
     <Provider store={store}>
       <NavigationContainer>
         <AppNavigator />
       </NavigationContainer>
     </Provider>
-  );
-};
+  </NativeBaseProvider>
+);
 
 export default App;
